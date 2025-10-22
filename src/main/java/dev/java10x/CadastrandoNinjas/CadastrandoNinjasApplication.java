f4ab0e85828796ff7b3c9a20790f5481f5c86863
AaronSena <aaronmagalhaes1@gmail.com>
2025-10-22 17:53:55 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastrandoNinjas/CadastrandoNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastrandoNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrandoNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrandoNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastrandoNinjas/CadastrandoNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastrandoNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrandoNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
