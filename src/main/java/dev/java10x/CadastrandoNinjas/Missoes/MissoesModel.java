7fd6aba3cc475036b5aea13401287876ffad18c5
AaronSena <aaronmagalhaes1@gmail.com>
2025-11-24 13:16:15 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/dev/java10x/CadastrandoNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastrandoNinjas/Missoes/MissoesModel.java
@@ -2,12 +2,18 @@ package dev.java10x.CadastrandoNinjas.Missoes;
 
 import dev.java10x.CadastrandoNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,45 +22,7 @@ public class MissoesModel {
     private String dificuldade;
 
     // Uma missão poderá ter varios ninjas atribuidos
-    @OneToMany(mappedBy = "missoes )
+    @OneToMany(mappedBy = "missoes")
     private List <NinjaModel> ninjas ;
-
-// --------------------- CONSTRUCTORS --------------------------
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String nomeMissao, String dificuldade) {
-        this.id = id;
-        this.nomeMissao = nomeMissao;
-        this.dificuldade = dificuldade;
-    }
-
-// --------------------- GETTERS e SETTERS --------------------------
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNomeMissao() {
-        return nomeMissao;
-    }
-
-    public void setNomeMissao(String nomeMissao) {
-        this.nomeMissao = nomeMissao;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 
 }
--- src/main/java/dev/java10x/CadastrandoNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastrandoNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,15 @@ package dev.java10x.CadastrandoNinjas.Ninjas;
 
 import dev.java10x.CadastrandoNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
-@Table(name = "cadastro_de_ninjas")
+@Table(name = "cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,44 +23,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id") // CHAVE ESTRANGEIRA
     private MissoesModel missoes;
-
-// --------------------- CONSTRUCTORS --------------------------
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-
-// --------------------- GETTERS e SETTERS --------------------------
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
